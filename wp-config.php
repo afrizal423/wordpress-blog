ede1cc042fc0cdfa2592c45ccf6384b532537b70
afrizal <afrizal2499@gmail.com>
2019-05-02 12:17:29 +0700

    progress ke2

--- wp-config.php
+++ wp-config.php
@@ -20,13 +20,13 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'Nama database' );
+define( 'DB_NAME', 'afrizalm_wp712' );
 
 /** MySQL database username */
-define( 'DB_USER', 'Ini Username' );
+define( 'DB_USER', 'afrizalm_wp712' );
 
 /** MySQL database password */
-define( 'DB_PASSWORD', 'Ini Password' );
+define( 'DB_PASSWORD', 'v7p@6S.eD2' );
 
 /** MySQL hostname */
 define( 'DB_HOST', 'localhost' );
